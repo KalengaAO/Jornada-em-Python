ec728ab5e9b9c4d34f33a96fa1b6295cdacf7704
Antonio Pedro Kalenga <akalenga@c3r3s1.42luanda.com>
2025-08-28 21:03:46 +0100

    desenvolvendo o gerenciador de tarefa conceitos basicos de python

---
+++ Projectos_extras/gerenciador_de_tarefa.py
@@ -0,0 +1,9 @@
+print ("Bem vindo ao gerenciador de tarefa\n")
+print ("1 = Adicionar\n2 = Remover\n3 = Consultar\n4 = sair\n")
+
+while (1)
+	process = int(input("Digete o numero que corresponde ao processo:"))
+	if process == 1
+		tarefa = str(input("Digite a nova tarefa: "))
+		
+
