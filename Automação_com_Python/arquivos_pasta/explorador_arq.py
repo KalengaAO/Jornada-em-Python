5c8d09aaf7da63f27de2511bf3e4e6211d3e5be7
Antonio Pedro Kalenga <akalenga@c3r3s1.42luanda.com>
2025-08-29 00:13:35 +0100

    explorador de arquivo desenvolvido

---
+++ Automação_com_Python/arquivos_pasta/explorador_arq.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import sys
+
+entrada = str(input("Digite o caminho da pasta: ")).lower().strip()
+
+base = Path(entrada).expanduser()
+arquivos = []
+pasta = []
+
+if not base.exists():
+	print("Caminho inválido!")
+	sys.exit(0)
+
+with open("log_explorador.log", 'a') as log:
+	for item in base.iterdir():
+		if item.is_dir():
+			log.write(f"pasta: {item}\n")
+		elif item.is_file():
+			log.write(f"ficheiro: {item}\n")
+
+print(f"Director listado em: {base.resolve()}")
