5061bc7130bc2f84f01e12cc32917d38e839c093
antoniopedrocalengar7@gmail.com <antoniopedrocalengar7@gmail.com>
2025-08-29 16:03:39 +0100

    update em readme

--- Automação_com_Python/Interação_com_o_sistema_Operacional/modulosys.py
+++ Automação_com_Python/Interação_com_o_sistema_Operacional/modulosys.py
@@ -1,6 +1,12 @@
+import time
 import sys
 import os
+
+inicio = time.perf_counter()
 
 print(sys.platform)
 print(os.getlogin())
 os.system("ls")
+time.sleep(15)
+fim = time.perf_counter()
+print (f"Tempo de execução: {fim - inicio:.2f}s")
