3c7f42f2deba5cb957c9c25298a9ebfe27bc18c5
Antonio Pedro Kalenga <akalenga@c3r3s1.42luanda.com>
2025-08-29 10:39:58 +0100

    adicionando script de filtragem csv

--- Automação_com_Python/manipulação_de_dados_em_Arquivos/filtrar_csv.py
+++ Automação_com_Python/manipulação_de_dados_em_Arquivos/filtrar_csv.py
@@ -16,9 +16,20 @@ alunos_filtrado = []
 print("CAMPOS: nome\tidade\tcurso\temail")
 with open(arquivo, mode='r', newline='', encoding='utf-8') as filein:
 	reader = csv.DictReader(filein)
-	filtro = str(input("Digite o campo a ser filtrado: ")).strip().lower()
+	campo = str(input("Digite o campo a ser filtrado: ")).strip().lower()
+	sem_dupli = set()
 	for alunos in reader:
-		if alunos['curso'].lower() == filtro:
+		sem_dupli.add(alunos[campo].lower())
+	print(sem_dupli)
+	filtro = str(input(f"Digite o {campo} a ser filtrado: ")).strip().lower()
+
+"""O script esta meio com instruções repetidas"""
+"""para fins didaticos """
+
+with open(arquivo, mode='r', newline='', encoding='utf-8') as file:
+	reader = csv.DictReader(file)
+	for alunos in reader:
+		if alunos[campo].lower() == filtro:
 			alunos_filtrado.append(alunos)
 
 with open("filtro.csv", mode='w', newline='', encoding='utf-8') as fileout:
