9822f49096ea343c90dabab3d927dd9800dd0d6b
antoniopedrocalengar7@gmail.com <antoniopedrocalengar7@gmail.com>
2025-08-30 19:32:42 +0100

    script de automação usando conceitos basicos com o pyautogui

--- Automação_com_Python/selenium/usandpyautogri.py
+++ Automação_com_Python/selenium/usandpyautogri.py
@@ -3,4 +3,17 @@
 import pyautogui
 import time
 
+time.sleep(2)
+pyautogui.click(100, 200)
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+time.sleep(2)
+pyautogui.write("Automação com python")
+pyautogui.press("enter")
+time.sleep(2)
 
+for i in range(5):
+	screenshot = pyautogui.screenshot()
+	screenshot.save(f"captura_{i}.png")
+	time.sleep(5)
