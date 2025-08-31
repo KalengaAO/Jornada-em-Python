644446bf408ce1b13c0caca025638c0ef64d3539
antoniopedrocalengar7@gmail.com <antoniopedrocalengar7@gmail.com>
2025-08-31 12:26:40 +0100

    servidor web com flask method GET e POST

--- Web_com_Python/rotasgetandpost.py
+++ Web_com_Python/rotasgetandpost.py
@@ -6,27 +6,42 @@ app = Flask(__name__)
 
 @app.route("/")
 def bemvind():
-	return '''
-			<form action="saudacao" method="GET">
-			<input type="submit" value="redirecionar">
-			</form>
-		 '''
+	return f'''
+		<form action="saudacao" method="GET">
+		<h3>Bem vindo !</h3>
+		<input type="submit" value="redirecionar">
+		</form>
+		'''
+
+@app.route("/sisifo")
+def sesifivo():
+	return f'''
+		<form action="saudacao" method="GET">
+		<h1> came back again Sísifo! </h1>
+		<input type="submit" value ="redirecionar">
+		</form>
+		'''
 
 @app.route("/saudacao", methods=["GET", "POST"])
 def saudacao():
 	if request.method == "POST":
-		print ("Dados recebidos:", request.form)
 		nome = request.form.get("nome", "visitante")
-		return (f"Olá, {nome}!")
+		return f''' 
+			<form action="sisifo">
+				<h3>Olá {nome}!</h3>
+				Retornar: <input type="submit" value="redirecionar">
+			</form>
+			'''
 	return '''
 		<form method="POST"> 
+			<h2>Digite o nome</h2>
 			Nome: <input name="nome">
 			<input type="submit" value="enviar">
 		</form>
 		'''
 @app.errorhandler(404)
 def pagina_not_found(e):
-	return (f"<h1>Página não encontrada T_T 404</h2>\n<p>{e}</p>")
+	return (f"<h2>Página não encontrada T_T 404</h2>\n<p>{e}</p>")
 if __name__ == "__main__":
 
 	app.run(debug=True)
