c7cacfb7e8cf6466686565a65b04cd0f37e15686
antoniopedrocalengar7@gmail.com <antoniopedrocalengar7@gmail.com>
2025-09-01 16:49:47 +0100

    API RESTsfull com flask

--- Web_com_Python/cadastro_usuarios/app.py
+++ Web_com_Python/cadastro_usuarios/app.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+from flask import Flask, jsonify
+import routes
+
+app = Flask(__name__)
+app.register_blueprint(routes.bp)
+
+@app.errorhandler(404)
+def pagina_not_found(error):
+	return jsonify({"Erro": "Endpoint não encontrado!"}), 404
+
+@app.errorhandler(500)
+def erro_interno(error):
+	return jsonify({"erro": "Erro interno no servidor"}), 500
+
+if __name__ == "__main__":
+	app.run(debug=True)
--- Web_com_Python/cadastro_usuarios/models.py
+++ Web_com_Python/cadastro_usuarios/models.py
@@ -6,8 +6,48 @@ import json
 arquivo = Path("usuarios.json")
 
 def carregar():
-	if arquivo.exists()
+	if arquivo.exists():
 		try:
-			with arquivo.open(mode='r', encoding'utf-8') as file:
-			return json.load(file)
+			with arquivo.open(mode='r', encoding='utf-8') as file:
+				return json.load(file)
+		except json.JSONDecodeError:
+			return []
+	return []
 
+def salvar(users):
+	with arquivo.open(mode='w', encoding='utf-8') as file:
+		json.dump(users, file, ensure_ascii=False, indent=2)
+
+def lista():
+	return carregar()
+
+def obter(id_):
+	users = carregar()
+	return next((u for u in users if u["id"] == id_), None)
+
+def criar(nome, email):
+	users = carregar()
+	novo = {"id":len(users) + 1, "nome": nome, "email": email}
+	users.append(novo)
+	salvar(users)
+	return novo
+
+def atualizar(id_, nome=None, email=None):
+	users = carregar()
+	user = next((u for u in users if u["id"] == id_), None)
+	if not user:
+		return None
+	if nome:
+		user["nome"] = nome
+	if email:
+		user["email"] = email
+	salvar(users)
+	return user 
+
+def remover(id_):
+	users = carregar()
+	user = next((u for u in users if u["id"] == id_), None)
+	if not user:
+		return None
+	users.remove(user)
+	return user
