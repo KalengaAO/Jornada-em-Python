67f9423527e98dbae13a0f3675670a08cf1c7ce4
antoniopedrocalengar7@gmail.com <antoniopedrocalengar7@gmail.com>
2025-09-01 12:06:31 +0100

    API RESTs estudanto

--- Web_com_Python/dia2/api_get_pos_delete.py
+++ Web_com_Python/dia2/api_get_pos_delete.py
@@ -5,39 +5,41 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 tarefas = [
-			{"id":1, "tarefas": "testando api"},
-			{"id":2, "tarefas": "testando o metodo delete"}
-		]
+		{"id":1, "tarefa":"Estudar programção"},
+		{"id":2, "tarefa":"Estudar rede"}]
 
-@app.route("/tarefas/<int:id_>", methods=["GET", "POST", "DELETE"])
-def gerenciador_de_tarefa(id_):
-	if not tarefas:
-		return jsonify({"erro": "Tarefas não encontrada"}), 404
-	for tarefa in tarefas:
-		if tarefa["id"] == id_:
-			break ;
-		else:
-			tarefa = {"id": None, "tarefa": None}
+@app.route("/tarefas", methods=["GET", "POST"])
+def get_and_post_tarefas():
 	if request.method == "GET":
-		return jsonify(tarefa)
+		return jsonify(tarefas)
 	elif request.method == "POST":
-		dados = request.get_json()
-		novo_obj = {"id": len(tarefas) + 1, "titulo": dados["titulo"]}
+		data = request.get_json()
+		novo_obj = {"id": len(tarefas) + 1, "tarefa":data["tarefa"]}
 		tarefas.append(novo_obj)
 		return jsonify(tarefas)
+
+@app.route("/tarefas/<int:id_>", methods=["GET", "PUT", "DELETE"])
+def gerenciador_tarefa(id_):
+	tarefa = {"id": None, "titulo": None}
+	for tarefa in tarefas:
+		if tarefa["id"] == id_:
+			break
+	if request.method == "GET":
+		return jsonify(tarefa)
+	elif request.method == "PUT":
+		dados = request.get_json()
+		tarefa["tarefa"] = dados.get("tarefa", tarefa["tarefa"])
+		return jsonify({"Mensagem": f"Tarefa com id {id_} actualiza!","tarefa":tarefa})
 	elif request.method == "DELETE":
-		if tarefa["id"] == None:
-			return jsonify({"mensagem": f"Id {id_ } inválido"})
 		tarefas.remove(tarefa)
-		return jsonify({"mensagem": f"Tarefa {id_} removidas"})
-	return jsonify({"mensagem": f"Tarefas {id_} não identificada"})
+		return jsonify(tarefas)
 
 @app.errorhandler(404)
-def	pagin_not_found(error):
-	return f"""
-			<h1 style="color:red;">Pagina inexistente neste servidor!</h1>
-			<a href="/tarefas/2" style="color:blue;">
-			clique aqui para uma pagina valida!</a>
-		"""
+def pagina_not_found(error):
+	return """
+		<h1 style="color:red;">Página inexistente neste servidor!</h1>
+		<a href="/tarefas" style="color:blue;">Clique aqui para uma página válida!</a>
+		""", 404
+
 if __name__ == "__main__":
 	app.run(debug=True)
