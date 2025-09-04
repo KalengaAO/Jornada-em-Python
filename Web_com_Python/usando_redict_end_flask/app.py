46771e3361dcd4036dbc1ecd25d4dd506ec8820d
antoniopedrocalengar7@gmail.com <antoniopedrocalengar7@gmail.com>
2025-09-04 14:49:34 +0100

    cadastro de usuarios

--- Web_com_Python/usando_redict_end_flask/app.py
+++ Web_com_Python/usando_redict_end_flask/app.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from usur.routes import bp as user_bp
+from user.routes import user_bp
 
 app = Flask(__name__)
+app.secret_key = "1234"
+app.register_blueprint(user_bp)
 
-app.register_blueprint(user_bp, url_prefix="/user")
+@app.errorhandler(404)
+def pag_not_found(error):
+	return '''
+			<h1 style="color:red">A pagina solicitada não existe</h1>
+		'''
+
+@app.errorhandler(500)
+def error_interno(error):
+	return '''
+		<h2 style="color:red">Erro interno no servidor</h2>
+		'''
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- Web_com_Python/usando_redict_end_flask/user/routes.py
+++ Web_com_Python/usando_redict_end_flask/user/routes.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+
+from flask import Blueprint, request, redirect, flash, url_for
+from flask import render_template as template
+
+user_bp = Blueprint("user", __name__, template_folder="templates")
+
+users = []
+
+@user_bp.route("/cadastro", methods=["GET", "POST"])
+def	cadastro():
+	if request.method == "POST":
+		nome = request.form.get("nome")
+		email = request.form.get("email")
+		idade = request.form.get("idade")
+
+		error = []
+		if not nome:
+			error.append("O campo nome é obrigatório.")
+		if not email:
+			error.append("Digite o email válido.")
+		if not idade.isdigit():
+			error.append("A idade de ver um digito.")
+		if not int(idade) >= 15:
+			error.append("Só é permitido idade igual ou superior a 15 anos de idade.")
+		if error:
+			for er in error:
+				flash(er, "erro")
+		else:
+			user = {"nome": nome, "email": email, "idade": idade}
+			users.append(user)
+			flash("usuário cadastrado com sucesso!", "sucesso")
+			return redirect(url_for("user.lista"))
+	return template("cadastro.html")
+
+@user_bp.route("/lista")
+def lista():
+	return template("lista.html", users=users)
